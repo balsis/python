54811c0a89716d2ac6abe237349b71fdcc0538d9
Artur <artur.balsis@gmail.com>
2023-10-02 23:11:30 +0300

    wrappers

---
+++ Challenges/decorator/double_it.py
@@ -0,0 +1,20 @@
+def double_it(func):
+    def wrapper(*args, **kwargs):
+        return 2*func(*args, **kwargs)
+    return wrapper
+
+
+@double_it
+def multiply(num1, num2):
+    return num1 * num2
+
+res = multiply(9, 4) # произведение 9*4=36, но декоратор double_it удваивает это значение
+print(res)
+
+
+@double_it
+def get_sum(*args):
+    return sum(args)
+
+res = get_sum(1, 2, 3, 4, 5)
+print(res) # печатает 30
