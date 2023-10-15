afe9b7ebb40d3c97dad78211e63c1dddc60ccb9d
Artur <artur.balsis@gmail.com>
2023-10-15 21:20:49 +0300

    add selenium_1.6

--- Challenges/temp.py
+++ Challenges/temp.py
@@ -1,3 +1,15 @@
-a = [[[[9]]]]
-if type(a) == list:
-    print(*a)
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import math
+
+link = 'http://suninjuly.github.io/find_link_text'
+link_value = str(math.ceil(math.pow(math.pi, math.e)*10000))
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+finally:
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
