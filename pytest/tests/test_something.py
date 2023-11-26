9a0e77bbdb5ca4ffc39fa30e258018f57579f5b2
Artur <artur.balsis@gmail.com>
2023-11-26 17:52:38 +0300

    add pytest and modify structure

---
+++ pytest/tests/test_something.py
@@ -0,0 +1,11 @@
+import requests
+
+from configuration import SERVICE_URL
+
+from src.baseclasses.response import Response
+from src.schemas.post import POST_SCHEMA
+
+def test_getting_posts():
+    r = requests.get(url=SERVICE_URL)
+    response = Response(r)
+    response.assert_status_code(200)
